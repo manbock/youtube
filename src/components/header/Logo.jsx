6a3ddeb30743cbc5df1ece5bdb6a785e5e63206d
unknown <fhqhzkq99@gmail.com>
2024-02-17 01:40:26 +0900

    잔잔 디버깅

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Logo = () => {
+const Logo = ({toggleMenu}) => {
   return (
-   <h1 className='header__logo'>
-      <a href="/">
-         <em aria-hidden='true'></em>
-         <span>webs<br />youtube</span>
-      </a>
-   </h1>
+    <h1 className='header__logo'>
+        <Link to="/">
+            <em aria-hidden='true' onClick={toggleMenu}></em>
+            <span>webs<br />youtube</span>
+        </Link>
+    </h1>
   )
 }
 
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -20,7 +20,7 @@ const Search = () => {
     }, [searchID]); 
 
     const fetchVideos = (query, pageToken = '') => {
-        fetchFromAPI(`search?part=snippet&q=${query}&pageToken=${pageToken}`)
+        fetchFromAPI(`search?part=snippet&type=video&q=${query}&pageToken=${pageToken}`)
             .then((data)=> {
                 console.log(data)
                 setNextPageToken(data.nextPageToken);
